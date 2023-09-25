0be6d6039a9cdb143767aeae6b53d601b95d1657
Soporte comboplay <desarrollador5@comboplay.co>
2023-09-25 12:19:56 -0500

    Configurancion de la arquitectura del proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+<body>
+<div class="alert alert-danger" role="alert">
+    <i class="bi bi-airplane-fill"></i>
+    A simple danger alert—check it out!
+</div>
+</body>
+</html>
